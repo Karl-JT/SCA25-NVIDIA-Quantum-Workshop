076b588ede4a99c5a3e44d6c2015112783320a66
realJT <y.juntao@hotmail.com>
2025-03-08 20:40:35 +0800

    Update Dockerfile.sh

--- scripts/Dockerfile.sh
+++ scripts/Dockerfile.sh
@@ -11,18 +11,17 @@ ENV PYTHONDONTWRITEBYTECODE=1 \
 WORKDIR /workspace
 
 # Copy demo material to workspace
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/figure/ /workspace/figure/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/grovers/ /workspace/grovers/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/output/ /workspace/output/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/profile_util/ /workspace/profile_util
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/cudaq-qibolab-emulator-demo.ipynb /workspace/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/CUDAQ_example.ipynb /workspace/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/Makefile /workspace/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/Benchmarking_example.ipynb /workspace/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/example_output.tgz /workspace/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/make_profile_util_lib.sh /workspace/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/req.txt /workspace/
-COPY /srv/SCA25-NVIDIA-Quantum-Workshop/Pawsey/workshop_utils.py /workspace/
+COPY ./figure/ /workspace/figure/
+COPY ./grovers/ /workspace/grovers/
+COPY ./output/ /workspace/output/
+COPY ./profile_util/ /workspace/profile_util
+COPY ./CUDAQ_example.ipynb /workspace/
+COPY ./Makefile /workspace/
+COPY ./Benchmarking_example.ipynb /workspace/
+COPY ./example_output.tgz /workspace/
+COPY ./make_profile_util_lib.sh /workspace/
+COPY ./req.txt /workspace/
+COPY ./workshop_utils.py /workspace/
 
 # Install basic Python packages
 RUN pip install --upgrade pip && \
