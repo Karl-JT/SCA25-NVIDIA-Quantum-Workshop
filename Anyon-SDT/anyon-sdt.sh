31e3ab1df4b42c25c55214de133c7bd9205c4792
realJT <y.juntao@hotmail.com>
2025-03-09 14:04:50 +0800

    Update anyon-sdt.sh

--- Anyon-SDT/anyon-sdt.sh
+++ Anyon-SDT/anyon-sdt.sh
@@ -8,6 +8,6 @@ fi
 PORT=$1
 
 last_two_digits=${PORT: -2}
-device_index=$((10#$last_two_digits - 1))
+device_index=$((10#$last_two_digits % 8))
 
 docker run --gpus '"device='$device_index'"' --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p $PORT:$PORT --rm anyon-sdt:latest jupyter-lab --port=$PORT --ip=0.0.0.0 --allow-root
