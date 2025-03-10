26056ec2ebcd8fada437626df1e66da558e19a4a
Jiwon Yune <34701778+jwyune@users.noreply.github.com>
2025-03-10 08:09:54 +0800

    Update anyon-sdt.sh

--- Anyon-SDT/anyon-sdt.sh
+++ Anyon-SDT/anyon-sdt.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then 
+	echo "Usage: $0 <port_number>"
+	exit 1
+fi
+
+PORT=$1
+
+docker run --gpus 'device=0' --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p $PORT:$PORT --rm anyon-sdt jupyter-lab --port=$PORT --ip=0.0.0.0 --allow-root
